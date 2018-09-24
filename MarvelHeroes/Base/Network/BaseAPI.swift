469828aeb1f5c957067c3f6acd39be43b44dde7e
LuisHFerreira <luisferreira.dev@gmail.com>
2018-09-24 20:39:36 -0300

    create struct BaseAPI and include pods SwiftHash/Kingfisher

---
+++ MarvelHeroes/Base/Network/BaseAPI.swift
@@ -0,0 +1,19 @@
+//
+//  BaseAPI.swift
+//  MarvelHeroes
+//
+//  Created by Luis Ferreira on 9/24/18.
+//  Copyright © 2018 Luis Ferreira. All rights reserved.
+//
+
+import Foundation
+
+struct BaseAPI {
+    private let basePath = ""
+    private let publicKey = ""
+    private let privateKey = ""
+    
+    private func getCredentials() -> String {
+        return ""
+    }
+}
